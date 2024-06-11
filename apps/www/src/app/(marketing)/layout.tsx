5ca0c143f1b81adcf0fe298189c8d4d812fe607e
h5365jheqhjy6ydrt4 <123633582+h5365jheqhjy6ydrt4@users.noreply.github.com>
2024-06-11 07:07:57 +0100

    Update layout.tsx

--- apps/www/src/app/(marketing)/layout.tsx
+++ apps/www/src/app/(marketing)/layout.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 import { DocSearch } from '@/components/doc-search'
 import { MainNav } from '@/components/main-nav'
+import { GitHub } from '@/components/ui/icons'
 import { marketingConfig } from '@/config/marketing'
 import { siteConfig } from '@/config/site'
 
 type MarketingLayoutProps = {
   children: React.ReactNode
 }
-
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
@@ -20,12 +20,18 @@ export default async function MarketingLayout({
           <nav className="flex space-x-4 justify-center align-middle">
             <DocSearch />
 
-           
-          
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className="flex"
+            >
+              <GitHub className="h-6 w-6 my-auto" />
+              <span className="sr-only">GitHub</span>
+            </Link>
           </nav>
         </div>
       </header>
-
       <main className="flex-1">{children}</main>
     </>
   )
